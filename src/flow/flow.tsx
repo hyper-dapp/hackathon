e0f7572978b8b6f189dc3e69f7d27bf094b9f9bd
Gilbert <gilbertbgarza@gmail.com>
2022-03-19 22:05:16 -0500

    Pull flow code from ipfs

--- src/flow/flow.tsx
+++ src/flow/flow.tsx
@@ -5,14 +5,37 @@ import { cc } from "mithril-cc";
 import { Flow, makeFlow } from "../lib/flow";
 import { makeWalletConnector } from "../lib/wallet-connector";
 import { FlowUI } from "../components/flow-ui";
-import { plainText as GUESTBOOK_EXAMPLE } from "../../example-flows/guestbook.pl";
 
 const Flow = cc(function () {
   let flow: Flow | null;
+  let query = m.parseQueryString(window.location.search)
+  let noSuchFlow = false
+
   const wallet = makeWalletConnector();
 
   this.oncreate(async () => {
-    flow = await makeFlow(GUESTBOOK_EXAMPLE);
+    if (!query.cid) {
+      noSuchFlow = true;
+      return;
+    }
+
+    try {
+      const req = await fetch(`https://ipfs.io/ipfs/${query.cid}`);
+      if (req.status !== 200) {
+        throw new Error('bad_request');
+      }
+
+      const json = await req.json()
+      if (typeof json.logic !== 'string') {
+        throw new Error('bad_request');
+      }
+
+      flow = await makeFlow(json.logic);
+    }
+    catch(err) {
+      noSuchFlow = true;
+    }
+
     m.redraw();
   });
 
@@ -22,6 +45,7 @@ const Flow = cc(function () {
         <div class="flex flex-col items-center">
           {[
             // In an array to enable key
+            !noSuchFlow &&
             m(FlowUI, {
               key: flow?.id || -1,
               flow,
@@ -29,6 +53,14 @@ const Flow = cc(function () {
               className: "w-[50%] rounded-xl",
             }),
           ]}
+          {noSuchFlow &&
+            <div class="dark:text-red-300">
+              {query.cid
+                ? `No such flow for given CID (${query.cid})`
+                : 'No given flow CID.'
+              }
+            </div>
+          }
           <p class="text-white font-bold italic mt-2">Powered by HyperDapp</p>
         </div>
       </div>
