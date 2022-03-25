0d1793a7e4b162e51138fe419e9af8ce8e4d91f7
Gilbert <gilbertbgarza@gmail.com>
2022-03-25 14:40:05 -0500

    Center wallet buttons

--- front-end/src/components/wallet-button.tsx
+++ front-end/src/components/wallet-button.tsx
@@ -9,26 +9,26 @@ type Attrs = {
 export const WalletButton = ccs<Attrs>(({ wallet }) => {
   const state = wallet.state;
   return (
-    <h1 class="text-3xl font-bold underline">
+    <div class="flex flex-col items-center text-3xl font-bold">
       {state.type === "loading" && "Loading"}
-      {state.type === "init" && (
-        <div class="p-4">
-          <button
-            class={`m-2 ${btnClass({ size: "lg" })}`}
-            onclick={() => wallet.connectViaWalletConnect()}
-          >
-            Connect with Wallet Connect
-          </button>
-          <button
-            class={`m-2 ${btnClass({ size: "lg" })}`}
-            onclick={() => wallet.connectViaMetamask()}
-          >
-            Connect with Metamask
-          </button>
-        </div>
-      )}
+      {state.type === "init" &&
+        <button
+          class={`m-2 ${btnClass({ size: "lg" })}`}
+          onclick={() => wallet.connectViaWalletConnect()}
+        >
+          Connect with Wallet Connect
+        </button>
+      }
+      {state.type === "init" &&
+        <button
+          class={`m-2 ${btnClass({ size: "lg" })}`}
+          onclick={() => wallet.connectViaMetamask()}
+        >
+          Connect with Metamask
+        </button>
+      }
       {state.type === "ready" && `Ready (${wallet.type})`}
       {state.type === "error" && `Error ${state.error.message}`}
-    </h1>
+    </div>
   );
 });
